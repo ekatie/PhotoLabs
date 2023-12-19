67de17cd0e5b9869bacf60be75cb3f96fb338730
Katie K <katiekucsera@gmail.com>
2023-12-19 18:32:39 -0500

    Import photo list item and display sample data

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import "../styles/PhotoList.scss";
+import PhotoListItem from "./PhotoListItem";
 
 const sampleDataForPhotoList = [
   {
@@ -59,7 +60,15 @@ const sampleDataForPhotoList = [
 const PhotoList = () => {
   return (
     <ul className="photo-list">
-      {/* Insert React */}
+      {sampleDataForPhotoList.map(({ id, urls, user, location }) => (
+        <PhotoListItem
+          key={id}
+          imageSource={urls.regular}
+          profile={user.profile}
+          username={user.username}
+          location={location}
+        />
+      ))}
     </ul>
   );
 };
