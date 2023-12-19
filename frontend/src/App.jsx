0a15aaf53a49e8a21fef89956cd51eec06b64ac3
Katie K <katiekucsera@gmail.com>
2023-12-19 18:11:32 -0500

    Comment out sample data to prepare for data input

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,23 +2,22 @@ import React from 'react';
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
+// const sampleDataForPhotoListItem = {
+//   id: "1",
+//   location: {
+//     city: "Montreal",
+//     country: "Canada",
+//   },
+//   imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+//   username: "Joe Example",
+//   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+// };
 
-const App = () => {
-  return (
-    <div className="App">
-      <PhotoListItem key={sampleDataForPhotoListItem.id} location={sampleDataForPhotoListItem.location} imageSource={sampleDataForPhotoListItem.imageSource} username={sampleDataForPhotoListItem.username} profile={sampleDataForPhotoListItem.profile}/>
-    </div>
-  );
-};
+const App = () => (
+  <div className="App">
+    {/* { Array.from(Array(3)).map((_, index) => <PhotoListItem key={index}/>) } */}
+    <PhotoList/>
+  </div>
+)
 
 export default App;
