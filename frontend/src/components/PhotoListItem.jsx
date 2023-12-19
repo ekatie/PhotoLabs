c714b62532388c9d26b96275ab552e641951e38c
Katie K <katiekucsera@gmail.com>
2023-12-19 18:33:26 -0500

    Refactor component to show sample data

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,11 +2,10 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 import PhotoFavButton from "./PhotoFavButton";
 
-const PhotoListItem = ({ data: { imageSource, profile, username, location } }) => (
-  <article className="photo-list__item">
-    <PhotoFavButton/>
+const PhotoListItem = ({ imageSource, profile, username, location }) => (
+  <li className="photo-list__item">
+    <PhotoFavButton />
     <img className="photo-list__image" src={imageSource} alt="photo" />
-
     <div className="photo-list__user-details">
       <img className="photo-list__user-profile" src={profile} alt={`${username}'s profile`} />
       <div className="photo-list__user-info">
@@ -14,8 +13,7 @@ const PhotoListItem = ({ data: { imageSource, profile, username, location } }) =
         <p className="photo-list__user-location">{location.city}, {location.country}</p>
       </div>
     </div>
-  </article>
+  </li>
 );
-
 
 export default PhotoListItem;
