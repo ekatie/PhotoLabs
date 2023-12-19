ba8e557d256633ce89e3cadacd7ae810e1ae16de
Katie K <katiekucsera@gmail.com>
2023-12-19 18:13:13 -0500

    Update render based on props data

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import "../styles/PhotoListItem.scss";
+import PhotoFavButton from "./PhotoFavButton";
 
-const PhotoListItem = (props) => {
-return (
-<div>
-  <img src={props.imageSource}/>
-  <img src={props.profile}/>
-  <p>{props.username}</p>
-  <p>{props.location.city}, {props.location.country}</p>
-</div>
-)
-};
+const PhotoListItem = ({ data: { imageSource, profile, username, location } }) => (
+  <article className="photo-list__item">
+    <PhotoFavButton/>
+    <img className="photo-list__image" src={imageSource} alt="photo" />
+
+    <div className="photo-list__user-details">
+      <img className="photo-list__user-profile" src={profile} alt={`${username}'s profile`} />
+      <div className="photo-list__user-info">
+        <p>{username}</p>
+        <p className="photo-list__user-location">{location.city}, {location.country}</p>
+      </div>
+    </div>
+  </article>
+);
+
 
 export default PhotoListItem;
