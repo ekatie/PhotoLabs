1149b8cd9ac7a6217be3bd39d31a6bf166cfd099
Katie K <katiekucsera@gmail.com>
2023-12-20 18:21:23 -0500

    Add fav icon alert to add/remove favourites functions, update props

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -3,7 +3,18 @@ import PhotoListItem from "./PhotoListItem";
 
 import "../styles/PhotoList.scss";
 
-const PhotoList = ({photos, favouritePhotos, onAddFavourite, onRemoveFavourite}) => {
+const PhotoList = ({photos, favouritePhotos, onAddFavourite, onRemoveFavourite, onToggleIcon}) => {
+
+  const handleAddFavourite = (photo) => {
+    onAddFavourite(photo);
+    onToggleIcon();
+  }
+
+  const handleRemoveFavourite = (photo) => { 
+    onRemoveFavourite(photo);
+    onToggleIcon();
+  }
+
   return (
     <ul className="photo-list">
       {photos.map((photo) => (
@@ -14,7 +25,7 @@ const PhotoList = ({photos, favouritePhotos, onAddFavourite, onRemoveFavourite})
           username={photo.user.username}
           location={photo.location}
           isFavourite={favouritePhotos.includes(photo.id)}
-          onToggleFavourite={(isFavourite) => isFavourite ? onAddFavourite(photo) : onRemoveFavourite(photo)}
+          onToggleFavourite={(isFavourite) => isFavourite ? handleAddFavourite(photo) : handleRemoveFavourite(photo)}
         />
       ))}
     </ul>
