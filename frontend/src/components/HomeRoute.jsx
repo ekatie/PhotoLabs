cc7976a4b62d713f3eb67dd535c1da00effd8333
Katie K <katiekucsera@gmail.com>
2023-12-20 15:59:14 -0500

    Manage photo favourite states, pass as props

--- frontend/src/components/HomeRoute.jsx
+++ frontend/src/components/HomeRoute.jsx
@@ -1,15 +1,32 @@
 import React from "react";
 import TopNavigation from "./TopNavigationBar";
 import PhotoList from "./PhotoList";
+import { useState } from "react";
 
 import '../styles/HomeRoute.scss'
 
 const HomeRoute = ({topics, photos}) => {
-  console.log(photos)
+
+const [favouritePhotos, setFavouritePhotos] = useState([])
+
+const addFavouritePhoto = (photo) => {
+  (console.log('Adding photo to favourites'))
+  setFavouritePhotos((prevPhotos) => [...prevPhotos, photo.id])
+}
+
+const removeFavouritePhoto = (photo) => {
+  console.log('Removing photo from favourites')
+  setFavouritePhotos((prevPhotos) => prevPhotos.filter(id => id !== photo.id))
+}
+
 return (
   <div className="home-route">
   <TopNavigation topics={topics}/>
-  <PhotoList photos={photos}/>
+  <PhotoList 
+  photos={photos}
+  favouritePhotos={favouritePhotos}
+  onAddFavourite={(photo) => addFavouritePhoto(photo)}
+  onRemoveFavourite={(photo) => removeFavouritePhoto(photo)}/>
   </div>
 )
 };
