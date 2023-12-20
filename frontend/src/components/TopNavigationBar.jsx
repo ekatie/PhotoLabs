8a496f34205991765047b3de39b6bc5ecf0b9ae3
Katie K <katiekucsera@gmail.com>
2023-12-20 18:21:52 -0500

    Update incoming and passed props for display alert

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TopicList from './TopicList';
 import FavIcon from './FavIcon';
 import '../styles/TopNavigationBar.scss'
 
-const TopNavigation = ({topics}) => {
+const TopNavigation = ({topics, displayAlert}) => {
+
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
       <TopicList topics={topics}/>
-      <FavIcon selected={true}/>
+      <FavIcon selected={true} displayAlert={displayAlert}/>
     </div>
   )
 }
