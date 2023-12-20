b9ed9a5cf058cbb44b16cf3bb6e5569c72e06bc4
Katie K <katiekucsera@gmail.com>
2023-12-20 14:23:46 -0500

    Import necessary components, add and use props in child components

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-
+import TopicList from './TopicList';
+import FavIcon from './FavIcon';
 import '../styles/TopNavigationBar.scss'
 
-const TopNavigation = () => {
+const TopNavigation = ({topics}) => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
+      <TopicList topics={topics}/>
+      <FavIcon selected={true}/>
     </div>
   )
 }
