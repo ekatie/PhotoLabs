50890aeb81699d80cb0b60d57c076c017f4caeec
Katie K <katiekucsera@gmail.com>
2023-12-20 15:58:17 -0500

    Organize imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import HomeRoute from 'components/HomeRoute';
-import './App.scss';
 import photos from './mocks/photos.js';
 import topics from 'mocks/topics';
+import './App.scss';
 
 const App = () => (
   <div className="App">
-    {/* { Array.from(Array(3)).map((_, index) => <PhotoListItem key={index}/>) } */}
   <HomeRoute photos={photos} topics={topics}/>
   </div>
 )
