7595b3208009d4418205b04bc9bf2cb149f73a59
Katie K <katiekucsera@gmail.com>
2023-12-21 17:17:58 -0500

    Add close function to modal component, display photo

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -3,12 +3,17 @@ import React from 'react';
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 
-const PhotoDetailsModal = () => {
+const PhotoDetailsModal = ({isModalOpen, onClose, content}) => {
+  if (!isModalOpen) return null;
+
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button">
+      <button className="photo-details-modal__close-button" onClick={onClose}>
         <img src={closeSymbol} alt="close symbol" />
       </button>
+      <div className="photo-details-modal__content">
+        {content}
+      </div>
     </div>
   )
 };
