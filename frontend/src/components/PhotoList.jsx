7934e4b41d4f703767d9e6c5e251ab7bee8c9771
Katie K <katiekucsera@gmail.com>
2023-12-21 17:15:45 -0500

    Feed modal handling into photo item for click handling

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -3,7 +3,7 @@ import PhotoListItem from "./PhotoListItem";
 
 import "../styles/PhotoList.scss";
 
-const PhotoList = ({photos, favouritePhotos, onAddFavourite, onRemoveFavourite, onToggleIcon}) => {
+const PhotoList = ({photos, favouritePhotos, onAddFavourite, onRemoveFavourite, onToggleIcon, onOpenModal}) => {
 
   const handleAddFavourite = (photo) => {
     onAddFavourite(photo);
@@ -16,19 +16,20 @@ const PhotoList = ({photos, favouritePhotos, onAddFavourite, onRemoveFavourite, 
   }
 
   return (
-    <ul className="photo-list">
-      {photos.map((photo) => (
-        <PhotoListItem
-          key={photo.id}
-          imageSource={photo.urls.regular}
-          profile={photo.user.profile}
-          username={photo.user.username}
-          location={photo.location}
-          isFavourite={favouritePhotos.includes(photo.id)}
-          onToggleFavourite={(isFavourite) => isFavourite ? handleAddFavourite(photo) : handleRemoveFavourite(photo)}
-        />
-      ))}
-    </ul>
+      <ul className="photo-list">
+        {photos.map((photo) => (
+          <PhotoListItem
+            key={photo.id}
+            imageSource={photo.urls.regular}
+            profile={photo.user.profile}
+            username={photo.user.username}
+            location={photo.location}
+            isFavourite={favouritePhotos.includes(photo.id)}
+            onToggleFavourite={(isFavourite) => isFavourite ? handleAddFavourite(photo) : handleRemoveFavourite(photo)}
+            onClick={() => onOpenModal(photo)}
+          />
+        ))}
+      </ul>
   );
 };
 
