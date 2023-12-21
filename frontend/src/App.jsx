59b067044c5bc2cfc03f702007a16ec6a9eb54a7
Katie K <katiekucsera@gmail.com>
2023-12-21 17:14:02 -0500

    Add state handling for photo modal and feed as props to child components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import { React, useState } from 'react';
 import HomeRoute from 'components/HomeRoute';
 import photos from './mocks/photos.js';
 import topics from 'mocks/topics';
 import './App.scss';
+import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 
-const App = () => (
+const App = () => {
+
+const [isModalOpen, setIsModalOpen] = useState(false);
+const [selectedPhoto, setSelectedPhoto] = useState(null);
+
+const openModalWithPhoto = (photo) => {
+  setSelectedPhoto(photo);
+  setIsModalOpen(true);
+}
+
+const closeModal = () => {
+  setIsModalOpen(false);
+}
+
+return (
   <div className="App">
-  <HomeRoute photos={photos} topics={topics}/>
+  <HomeRoute photos={photos} topics={topics} onOpenModal={openModalWithPhoto}/>
+  <PhotoDetailsModal 
+      isModalOpen={isModalOpen}
+      onClose={closeModal}
+      content={selectedPhoto && (
+        <div>
+          <h2>{selectedPhoto.user.username}</h2>
+          <img src={selectedPhoto.urls.regular} alt="Selected Photo" />
+        </div>
+      )}
+      />
   </div>
 )
+}
 
 export default App;
