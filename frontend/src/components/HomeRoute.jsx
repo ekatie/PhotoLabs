f0743b89f261738506d070dc19081837f71cabb4
Katie K <katiekucsera@gmail.com>
2023-12-22 12:43:29 -0500

    Move state management to top level and use props

--- frontend/src/components/HomeRoute.jsx
+++ frontend/src/components/HomeRoute.jsx
@@ -1,50 +1,32 @@
 import React from "react";
 import TopNavigation from "./TopNavigationBar";
 import PhotoList from "./PhotoList";
-import { useState, useEffect } from "react";
 
-import '../styles/HomeRoute.scss'
+import "../styles/HomeRoute.scss";
 
-const HomeRoute = ({topics, photos, onOpenModal}) => {
-
-const [favouritePhotos, setFavouritePhotos] = useState([])
-const [displayAlert, setDisplayAlert] = useState(false)
-
-useEffect(() => {
-  toggleDisplayAlert();
-}, [favouritePhotos]);
-
-const addFavouritePhoto = (photo) => {
-  setFavouritePhotos((prevPhotos) => {
-    const newPhotos = [...prevPhotos, photo.id];
-    return newPhotos;
-  });
-}
-
-const removeFavouritePhoto = (photo) => {
-  setFavouritePhotos((prevPhotos) => {
-    const newPhotos = prevPhotos.filter(id => id !== photo.id);
-    return newPhotos;
-  });
-}
-
-const toggleDisplayAlert = () => {
- return favouritePhotos.length === 0 ? setDisplayAlert(false) : setDisplayAlert(true)
-}
-
-return (
-  <div className="home-route">
-  <TopNavigation topics={topics} displayAlert={displayAlert}/>
-  <PhotoList 
-  photos={photos}
-  favouritePhotos={favouritePhotos}
-  onAddFavourite={(photo) => addFavouritePhoto(photo)}
-  onRemoveFavourite={(photo) => removeFavouritePhoto(photo)}
-  onToggleIcon={toggleDisplayAlert} 
-  onOpenModal={onOpenModal}
-  />
-  </div>
-)
+const HomeRoute = ({
+  topics,
+  photos,
+  onOpenModal,
+  favouritePhotos,
+  displayAlert,
+  addFavouritePhoto,
+  removeFavouritePhoto,
+  toggleDisplayAlert,
+}) => {
+  return (
+    <div className="home-route">
+      <TopNavigation topics={topics} displayAlert={displayAlert} />
+      <PhotoList
+        photos={photos}
+        favouritePhotos={favouritePhotos}
+        addFavouritePhoto={(photo) => addFavouritePhoto(photo)}
+        removeFavouritePhoto={(photo) => removeFavouritePhoto(photo)}
+        onToggleIcon={toggleDisplayAlert}
+        onOpenModal={onOpenModal}
+      />
+    </div>
+  );
 };
 
 export default HomeRoute;
