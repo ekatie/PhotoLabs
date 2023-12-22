ec908cb5e47d0333e7438686273c72c4d39af144
Katie K <katiekucsera@gmail.com>
2023-12-22 12:44:56 -0500

    Refactor code to match wireframe display with scss classes

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -3,12 +3,22 @@ import React from "react";
 import "../styles/PhotoDetailsModal.scss";
 import closeSymbol from "../assets/closeSymbol.svg";
 import PhotoFavButton from "../components/PhotoFavButton";
-import PhotoList from "components/PhotoList";
 
-const PhotoDetailsModal = ({ isModalOpen, onClose, photo, photos }) => {
+const PhotoDetailsModal = ({
+  isModalOpen,
+  onClose,
+  photo,
+  photos,
+  onToggleFavourite,
+}) => {
   if (!isModalOpen || !photo) return null;
 
-  const { imageSource, profile, username, location } = photo;
+  const { imageSource, profile, username, location, isFavourite } = photo;
+
+  const handleToggleFavourite = (event) => {
+    event.stopPropagation();
+    onToggleFavourite(!isFavourite);
+  };
 
   return (
     <>
@@ -16,34 +26,42 @@ const PhotoDetailsModal = ({ isModalOpen, onClose, photo, photos }) => {
         <button className="photo-details-modal__close-button" onClick={onClose}>
           <img src={closeSymbol} alt="Close" />
         </button>
-        <PhotoFavButton
-        // isFavourite={isFavourite}
-        // onToggleFavourite={(event) => handleToggleFavourite(event)}
-        />
-        <img
-          className="photo-details-modal__image"
-          src={imageSource}
-          alt="Selected Photo"
-        />
         <div className="photo-details-modal__top-bar">
-          <div className="photo-details-modal__photographer-details">
-            <img
-              className="photo-details-modal__photographer-profile"
-              src={profile}
-              alt={"User Profile"}
+          <div>
+            <PhotoFavButton
+              isFavourite={isFavourite}
+              onToggleFavourite={(event) => handleToggleFavourite(event)}
             />
-            <div className="photo-details-modal__photographer-info">
-              <p>{username}</p>
+            <div>
+              <img
+                className="photo-details-modal__image"
+                src={imageSource}
+                alt="Selected Photo"
+              />
+            </div>
+            <div>
+              <div className="photo-details-modal__photographer-details">
+                <img
+                  className="photo-details-modal__photographer-profile"
+                  src={profile}
+                  alt={"User Profile"}
+                />
+                <div className="photo-details-modal__photographer-info">
+                  <p>{username}</p>
 
-              <p className="photo-details-modal__photographer-location">
-                {location.city}, {location.country}
-              </p>
+                  <p className="photo-details-modal__photographer-location">
+                    {location.city}, {location.country}
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div>
+              <h2 className="photo-details-modal__header">Similar Photos</h2>
+              <div className="photo-details-modal__images">
+                {/* <PhotoList photos={photos}/> */}
+              </div>
             </div>
           </div>
-        </div>
-        <h2 className="photo-details-modal__header">Similar Photos</h2>
-        <div className="photo-details-modal__images">
-          {/* <PhotoList photos={photos}/> */}
         </div>
       </div>
     </>
