cc1ec834bb349ca3db30a11a3bfe60d2ab87631c
Katie K <katiekucsera@gmail.com>
2023-12-23 17:42:58 -0500

    Remove unnecessary imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { React, useState, useEffect } from "react";
+import React from "react";
 import HomeRoute from "components/HomeRoute";
 import photos from "mocks/photos";
 import topics from "mocks/topics";
