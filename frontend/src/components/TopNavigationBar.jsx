eaf735192d0093058f566c8716679af4ef5f0344
Katie K <katiekucsera@gmail.com>
2023-12-29 15:25:16 -0500

    Add function to sort by topic to props

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,17 +1,16 @@
-import React, { useState } from 'react';
-import TopicList from './TopicList';
-import FavIcon from './FavIcon';
-import '../styles/TopNavigationBar.scss'
+import React, { useState } from "react";
+import TopicList from "./TopicList";
+import FavIcon from "./FavIcon";
+import "../styles/TopNavigationBar.scss";
 
-const TopNavigation = ({topics, displayAlert}) => {
-
+const TopNavigation = ({ topics, displayAlert, getPhotosByTopic }) => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
-      <TopicList topics={topics}/>
-      <FavIcon selected={true} displayAlert={displayAlert}/>
+      <TopicList topics={topics} getPhotosByTopic={getPhotosByTopic} />
+      <FavIcon selected={true} displayAlert={displayAlert} />
     </div>
-  )
-}
+  );
+};
 
 export default TopNavigation;
