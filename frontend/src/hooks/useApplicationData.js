24bb996edcb1a1b9759336db97831cdff9095ea1
Katie K <katiekucsera@gmail.com>
2023-12-29 15:27:19 -0500

    Replace mock data with useEffect API calls to db for photos and topics

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -5,7 +5,11 @@ export const ACTIONS = {
   CLOSE_MODAL: 'CLOSE_MODAL',
   ADD_FAVOURITE: 'ADD_FAVOURITE',
   REMOVE_FAVOURITE: 'REMOVE_FAVOURITE',
-  TOGGLE_ALERT: 'TOGGLE_ALERT'
+  TOGGLE_ALERT: 'TOGGLE_ALERT',
+  SET_TOPICS: 'SET_TOPICS',
+  SET_PHOTOS: 'SET_PHOTOS',
+  GET_PHOTOS_BY_TOPIC: 'GET_PHOTOS_BY_TOPIC',
+  SET_TOPIC_ID: 'SET_TOPIC_ID',
 };
 
 const initialState = {
@@ -13,6 +17,9 @@ const initialState = {
   selectedPhoto: null,
   favouritePhotos: [],
   displayAlert: false,
+  photoData: [],
+  topicData: [],
+  topicId: null,
 };
 
 function reducer(state, action) {
@@ -28,6 +35,14 @@ function reducer(state, action) {
       return { ...state, favouritePhotos: newPhotos };
     case ACTIONS.TOGGLE_ALERT:
       return { ...state, displayAlert: state.favouritePhotos.length !== 0 };
+    case ACTIONS.SET_TOPICS:
+      return { ...state, topicData: action.payload };
+    case ACTIONS.SET_PHOTOS:
+      return { ...state, photoData: action.payload };
+    case ACTIONS.GET_PHOTOS_BY_TOPIC:
+      return { ...state, photoData: action.payload };
+    case ACTIONS.SET_TOPIC_ID:
+      return { ...state, topicId: action.payload };
     default:
       return state;
   }
@@ -53,9 +68,47 @@ const useApplicationData = () => {
     dispatch({ type: ACTIONS.REMOVE_FAVOURITE, photo });
   };
 
+  // Show alert when there are favourited photos
   useEffect(() => {
     dispatch({ type: ACTIONS.TOGGLE_ALERT });
   }, [state.favouritePhotos]);
+
+  // Fetch topics from database
+  useEffect(() => {
+    fetch('/api/topics')
+      .then(response => response.json())
+      .then(data => {
+        dispatch({ type: ACTIONS.SET_TOPICS, payload: data });
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  // Fetch photos from database
+  useEffect(() => {
+    fetch('/api/photos')
+      .then(response => response.json())
+      .then(data => {
+        dispatch({ type: ACTIONS.SET_PHOTOS, payload: data });
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  // Update topic id in state
+  const getPhotosByTopic = (id) => {
+    dispatch({ type: ACTIONS.SET_TOPIC_ID, payload: id });
+  };
+
+  // Fetch photos by topic id
+  useEffect(() => {
+    if (state.topicId !== null) {
+      fetch(`/api/topics/photos/${state.topicId}`)
+        .then(response => response.json())
+        .then(data => {
+          dispatch({ type: ACTIONS.GET_PHOTOS_BY_TOPIC, payload: data });
+        })
+        .catch(err => console.log(err));
+    }
+  }, [state.topicId]);
 
   return {
     state,
@@ -63,6 +116,7 @@ const useApplicationData = () => {
     closeModal,
     addFavouritePhoto,
     removeFavouritePhoto,
+    getPhotosByTopic,
   };
 
 };
