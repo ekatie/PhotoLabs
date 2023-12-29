9c03d55e6d7b82b6c238e829cbee478135fe6447
Katie K <katiekucsera@gmail.com>
2023-12-29 15:26:24 -0500

    Add onClick event listener and implement function sort photos by topic

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 import "../styles/TopicListItem.scss";
 
-const TopicListItem = ({id, title}) => {
+const TopicListItem = ({ id, title, getPhotosByTopic }) => {
   return (
     <div className="topic-list__item">
-    <span>{title}</span>
+      <span onClick={() => getPhotosByTopic(id)}>{title}</span>
     </div>
   );
 };
