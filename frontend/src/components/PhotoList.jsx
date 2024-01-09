0aacb1cd02b780bc88a0c91f0ade33130d0a3394
Katie K <katiekucsera@gmail.com>
2024-01-09 17:07:17 -0500

    Update displayed photos based on search term, error for no matches

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -3,32 +3,44 @@ import PhotoListItem from "./PhotoListItem";
 import "../styles/PhotoList.scss";
 
 const PhotoList = ({
-  photos,
   favouritePhotos,
   addFavouritePhoto,
   removeFavouritePhoto,
   onOpenModal,
   showFavourites,
+  filteredPhotos,
 }) => {
-  const displayedPhotos = showFavourites ? favouritePhotos : photos;
+  let displayedPhotos;
+  if (showFavourites) {
+    displayedPhotos = favouritePhotos;
+  } else {
+    displayedPhotos = filteredPhotos;
+  }
+
   return (
     <ul className="photo-list">
-      {displayedPhotos.map((photo) => (
-        <PhotoListItem
-          key={photo.id}
-          imageSource={photo.urls.regular}
-          profile={photo.user.profile}
-          username={photo.user.username}
-          location={photo.location}
-          isFavourite={favouritePhotos.some(
-            (favPhoto) => favPhoto.id === photo.id
-          )}
-          onToggleFavourite={(isFavourite) =>
-            isFavourite ? addFavouritePhoto(photo) : removeFavouritePhoto(photo)
-          }
-          onClick={() => onOpenModal(photo)}
-        />
-      ))}
+      {displayedPhotos.length > 0 ? (
+        displayedPhotos.map((photo) => (
+          <PhotoListItem
+            key={photo.id}
+            imageSource={photo.urls.regular}
+            profile={photo.user.profile}
+            username={photo.user.username}
+            location={photo.location}
+            isFavourite={favouritePhotos.some(
+              (favPhoto) => favPhoto.id === photo.id
+            )}
+            onToggleFavourite={(isFavourite) =>
+              isFavourite
+                ? addFavouritePhoto(photo)
+                : removeFavouritePhoto(photo)
+            }
+            onClick={() => onOpenModal(photo)}
+          />
+        ))
+      ) : (
+        <p className="error">Sorry - no photos match your search.</p>
+      )}
     </ul>
   );
 };
