071125657de92bceb819469dd60d6ee54d072776
Katie K <katiekucsera@gmail.com>
2024-01-09 16:04:46 -0500

    Use new props to display all or only favourite photos

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -8,17 +8,21 @@ const PhotoList = ({
   addFavouritePhoto,
   removeFavouritePhoto,
   onOpenModal,
+  showFavourites,
 }) => {
+  const displayedPhotos = showFavourites ? favouritePhotos : photos;
   return (
     <ul className="photo-list">
-      {photos.map((photo) => (
+      {displayedPhotos.map((photo) => (
         <PhotoListItem
           key={photo.id}
           imageSource={photo.urls.regular}
           profile={photo.user.profile}
           username={photo.user.username}
           location={photo.location}
-          isFavourite={favouritePhotos.includes(photo.id)}
+          isFavourite={favouritePhotos.some(
+            (favPhoto) => favPhoto.id === photo.id
+          )}
           onToggleFavourite={(isFavourite) =>
             isFavourite ? addFavouritePhoto(photo) : removeFavouritePhoto(photo)
           }
