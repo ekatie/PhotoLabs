29437daea8c7c8db46bc8b39c298c5dc7e9131cd
Katie K <katiekucsera@gmail.com>
2024-01-09 16:04:05 -0500

    Pass state and function for viewing favourites to child component

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,14 +1,25 @@
-import React, { useState } from "react";
+import React from "react";
 import TopicList from "./TopicList";
 import FavIcon from "./FavIcon";
+import SearchBar from "./SearchBar";
 import "../styles/TopNavigationBar.scss";
 
-const TopNavigation = ({ topics, displayAlert, getPhotosByTopic }) => {
+const TopNavigation = ({
+  topics,
+  displayAlert,
+  getPhotosByTopic,
+  toggleFavourites,
+}) => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
       <TopicList topics={topics} getPhotosByTopic={getPhotosByTopic} />
-      <FavIcon selected={true} displayAlert={displayAlert} />
+      <SearchBar />
+      <FavIcon
+        selected={true}
+        displayAlert={displayAlert}
+        toggleFavourites={toggleFavourites}
+      />
     </div>
   );
 };
