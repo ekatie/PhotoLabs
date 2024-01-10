96d9f4676b46b93dea3861989cac69f745fc69a4
Katie K <katiekucsera@gmail.com>
2024-01-10 16:51:06 -0500

    Add comments for clarity

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -2,6 +2,14 @@ import React from "react";
 import TopicListItem from "./TopicListItem";
 import "../styles/TopicList.scss";
 
+/**
+ * This component renders a list of topics.
+ * @param {Object} props
+ * @param {Object[]} props.topics - an array of topics
+ * @param {function} props.getPhotosByTopic - function to get photos by topic
+ * @returns {JSX.Element}
+ */
+
 const TopicList = ({ topics, getPhotosByTopic }) => {
   return (
     <div className="top-nav-bar__topic-list">
