3bf1ef5585b7f99b00d968770e56edfca9b2dae3
Katie K <katiekucsera@gmail.com>
2024-01-10 16:49:04 -0500

    Add comments for clarity

--- frontend/src/components/FavBadge.jsx
+++ frontend/src/components/FavBadge.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 import FavIcon from './FavIcon';
 import '../styles/FavBadge.scss';
 
+/**
+ * This component renders the favourite badge. It is used to indicate whether a photo is a favourite or not.
+ * @param {Object} props
+ * @param {boolean} props.isFavPhotoExist - whether the photo is a favourite or not
+ * @returns {JSX.Element}
+ */
+
 const FavBadge = ({ isFavPhotoExist }) => {
   return (
     <div className='fav-badge'>
