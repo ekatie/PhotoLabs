d054fc6fdbf34a24e7291b2ca51ca0afcf7884f1
Katie K <katiekucsera@gmail.com>
2024-01-10 16:51:20 -0500

    Add comments for clarity

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import "../styles/TopicListItem.scss";
 
+/**
+ * This component renders a topic in the list of topics.
+ * @param {Object} props
+ * @param {string} props.id - the id of the topic
+ * @param {string} props.title - the title of the topic
+ * @param {function} props.getPhotosByTopic - function to get photos by topic
+ * @returns {JSX.Element}
+ */
+
 const TopicListItem = ({ id, title, getPhotosByTopic }) => {
   return (
     <div className="topic-list__item">
