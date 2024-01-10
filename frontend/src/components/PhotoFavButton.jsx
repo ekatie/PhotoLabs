6dc2e023cf5b2a0922257cb8ae140247302b7a49
Katie K <katiekucsera@gmail.com>
2024-01-10 16:49:52 -0500

    Add comments for clarity

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -2,6 +2,14 @@ import React from "react";
 import FavIcon from "./FavIcon";
 import "../styles/PhotoFavButton.scss";
 
+/**
+ * This component renders the favourite button for a photo.
+ * @param {Object} props
+ * @param {boolean} props.isFavourite - whether the photo is a favourite or not
+ * @param {function} props.onToggleFavourite - function to toggle whether the photo is a favourite or not
+ * @returns {JSX.Element}
+ */
+
 function PhotoFavButton({ isFavourite, onToggleFavourite }) {
   return (
     <div className="photo-list__fav-icon" onClick={onToggleFavourite}>
