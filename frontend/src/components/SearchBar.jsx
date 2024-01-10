5857949c47965f5d66ecb45a72191ece2d632d0f
Katie K <katiekucsera@gmail.com>
2024-01-10 16:50:50 -0500

    Add comments for clarity

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import "../styles/SearchBar.scss";
 
+/**
+ * This component renders the search bar.
+ * @param {Object} props
+ * @param {function} props.setSearchTerm - function to set the search term
+ * @param {string} props.searchTerm - the search term
+ * @returns {JSX.Element}
+ */
+
 const SearchBar = ({ setSearchTerm, searchTerm }) => {
   return (
     <div className="search-bar">
